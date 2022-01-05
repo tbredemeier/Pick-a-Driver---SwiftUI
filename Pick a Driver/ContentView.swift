ad13234a744c398540c0d448caf1bad7b48f0323
Tom Bredemeier <tbredemeier@barrington220.org>
2022-01-05 08:15:44 -0600

    Initial Commit

---
+++ Pick a Driver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pick a Driver
+//
+//  Created by Tom Bredemeier on 1/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pick a Driver/Pick_a_DriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_DriverApp.swift
+//  Pick a Driver
+//
+//  Created by Tom Bredemeier on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_DriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
